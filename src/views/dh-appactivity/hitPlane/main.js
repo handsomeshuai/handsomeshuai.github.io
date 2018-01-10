49f3551b6ae595d7e2c45c45284c08b8a1b5c745
lishuaishua <lishuaishuai@51testing.com>
2018-01-10 16:26:36 +0800

    'idea test'

--- src/views/dh-appactivity/hitPlane/main.js
+++ src/views/dh-appactivity/hitPlane/main.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
 import hello from './components/hello.vue';
 import plane from './components/plane.vue';
-import './assets/index.less'
+import './assets/index.less';
+
+import img1 from './imgs/logo.jpg';
+import img2 from './imgs/plane.jpg';
+import img3 from './imgs/logo.jpg';
 
 new Vue({
     el:'#app',
     data:function () {
-        return{
-
-        }
+        return {
+            imgList:[
+                {
+                    src:img1
+                },
+                {
+                    src:img2
+                },
+                {
+                    src:img3
+                }
+            ]
+        };
     },
     components:{
         hello:hello,

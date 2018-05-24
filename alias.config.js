679bfd16e5b683a3590fffa9b49cf5b161c765ed
lishuaishua <lishuaishuai@51testing.com>
2018-05-24 16:32:01 +0800

    'idea test 222'

--- alias.config.js
+++ alias.config.js
@@ -2,7 +2,8 @@ let path = require('path');
 module.exports = {
     'vue': 'vue/dist/vue.js',
     'vue-router':'vue-router/dist/vue-router.js',
-    'echarts':'echarts/dist/echarts.js'
+    'echarts':'echarts/dist/echarts.js',
+    'express':''
     /* Const */
 
 };
